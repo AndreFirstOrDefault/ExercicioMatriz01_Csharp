cea50afe415da6616c5aab13626a275def7de07f
Andre Luiz <andre.java.ads.2022@gmal.com>
2023-04-19 20:05:32 -0300

    Enviando exercicio de Matriz

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ExercicioResolvido_Aula80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n,n];
+           
+            for(int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+              
+                for(int j = 0; j < n; j++)
+                {
+                    mat[i,j] = int.Parse(values[j]);
+                } 
+            }
+
+            Console.WriteLine("\nMain diagonal:");
+            int count = 0;
+            foreach(int i in mat)
+            {
+                if (mat[0,0] == i)
+                {
+                    Console.Write(mat[0,0] + " " + mat[1,1] + " " + mat[2,2] + "\n");
+                }
+                
+                if(i < 0)
+                {
+                    count++;
+                }
+                
+            }
+            Console.WriteLine("Negative numbers = " + count);
+            
+        }
+    }
+}
